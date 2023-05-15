5bf73622d64b65f2f589ba2ed75ea2df5056b0b7
HersonDRB <133415460+HersonDRB@users.noreply.github.com>
2023-05-14 23:36:37 -0500

    Reto1.java

--- Retos/Reto1.java
+++ Retos/Reto1.java
@@ -2,12 +2,11 @@ package Retos;
 
 public class Reto1 {
     public static void main(String[] rgs) {
-        // Declarar variables
+
         double temp = 350, centi = 0;
-        // Calcular temperatura en grados
+
+        System.out.println("Bienvenido al programa para convertir los grados Fahrenheit (°F) a Centígrados (°C)\ncon la siguiente operacion: (350-32)/1.8\n");
         centi = (temp - 32) / 1.8;
-        // Mostrar resultados
-        System.out.println("Bienvenido al programa para convertir los grados Fahrenheit (°F) a Centígrados (°C)\n");
 
         System.out.println("La temperatura para precalentar el horno es:" + centi);
     }
